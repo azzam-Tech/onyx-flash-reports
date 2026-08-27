cf7dbc38ee75d8fe7182536f7fc58c1000113af9
Azzam Omar Nasser Bajahaw <210280603@firat.edu.tr>
2026-08-27 13:49:58 +0300

    Clean up root directory by removing old temporary files

--- check.py
+++
@@ -1,6 +0,0 @@
-import oracledb
-oracledb.init_oracle_client(lib_dir=r'C:\oracle\instantclient\instantclient_23_0')
-con = oracledb.connect(user='RPT_USER', password='ULT2016', dsn='100.100.1.100:1521/ORCL')
-cur = con.cursor()
-cur.execute("SELECT column_name FROM all_tab_columns WHERE table_name = 'CUSTOMER' AND owner = 'IAS20261'")
-print([r[0] for r in cur.fetchall()])
